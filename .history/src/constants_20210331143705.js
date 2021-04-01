859830e9dbe7760007bd334f1037a6c28984f639
Velagapudi Surya Sandeep <64702635+suryasandeep3@users.noreply.github.com>
2021-04-01 11:29:42 +0530

    Delete .history/src directory

--- .history/src/constants_20210331143705.js
+++
@@ -1,9 +0,0 @@
-export const options = [
-    { label: "Hyderabad", value: "HYDERABAD" },
-    { label: "Chennai", value: "CHENNAI" },
-    { label: "Bangalore", value: "BANGALORE" },
-    { label: "Delhi", value: "DELHI" },
-    { label: "Mumbai", value: "MUMBAI" }
-];
-
-export const defaultDropdownValue = { label: "Hyderabad", value: "HYDERABAD" };
