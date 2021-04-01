f6dcf034d42648977e7628cb9fe46790a6c97d3a
suryasandeep3 <vss10@iitbbs.ac.in>
2021-04-01 10:53:45 +0530

    Bank Search

---
+++ .history/src/constants_20210331143705.js
@@ -0,0 +1,9 @@
+export const options = [
+    { label: "Hyderabad", value: "HYDERABAD" },
+    { label: "Chennai", value: "CHENNAI" },
+    { label: "Bangalore", value: "BANGALORE" },
+    { label: "Delhi", value: "DELHI" },
+    { label: "Mumbai", value: "MUMBAI" }
+];
+
+export const defaultDropdownValue = { label: "Hyderabad", value: "HYDERABAD" };
